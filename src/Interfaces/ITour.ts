80864815ec3fa5b4936d90b7e1bfc42f7c83e52b
Elnur <136242453+Elnurio95@users.noreply.github.com>
2026-02-25 17:25:40 +0300

    [Homework №16]: completed

---
+++ src/Interfaces/ITour.ts
@@ -0,0 +1,6 @@
+export interface ITour {
+    id: number, 
+    image: string,
+    title: string, 
+    description: string
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,92 @@
 import { Component } from '@angular/core';
 import { Color } from '../enums/Color';
+import { ITour } from '../Interfaces/ITour';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  standalone: true,
 })
-
 export class AppComponent {
-
   companyName: string = 'румтибет';
+
+  tourLocation: string = ''; 
+  tourDate: string = ''; 
+  tourParticipant: string = '';
+  
+   today: Date = new Date(); 
+
   constructor() {
     this.setLastVisit();
     this.setQuantity();
+    this.removedInscription();
+    this.increment(); 
+    this.decrement(); 
+    this.showTask(); 
+    this.removeTask(); 
+
+    setInterval(() => {
+      this.today = new Date();
+    }, 1000);
+
+    setInterval(() => {
+      this.isLoading = false; 
+    }, 2000)
+  }
+
+  isLoading: boolean = true; 
+  input: string = ''; 
+
+  tasks = 'Задачи'; 
+  count = false; 
+
+  showTask() {
+    return this.count = !this.count;  
+  }
+
+  removeTask() {
+    return this.count = false; 
+  }
+
+  sum = 0; 
+
+  increment(): number {   
+    return this.sum++; 
+  }
+
+  decrement(): number {
+    if (this.sum > 0) {
+      return this.sum--; 
+    } 
+
+    return this.sum; 
+  }
+
+  removedInscription(): void {
+    const dateInscription = document.querySelector('.date-inscription'); 
+    dateInscription?.classList.add('removeLettering');
+  }
+
+  get isFormInvalid(): boolean {
+    return !this.tourLocation || !this.tourDate || !this.tourParticipant;
+  }
+
+  animationIndex: number | null = null; 
+
+  onMouseOver(index: number) {
+    this.animationIndex = index; 
+    const animation = document.querySelector('.tour__content'); 
+    const tourTitle = document.querySelector('.tour-title'); 
+    tourTitle?.classList.add('tour-title');
+    animation?.classList.add('animation');
+  }
+
+  onMouseLeave() {
+    this.animationIndex = null; 
   }
 
   isColor(color: Color): boolean {
@@ -32,4 +105,27 @@ export class AppComponent {
     localStorage.setItem(key, String(visits));
   }
 
+  tours: ITour[] = [
+    {
+      id: 1,
+      image: './images/guide-trip-icon.png',
+      title: 'Опытный гид',
+      description:
+        'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+    },
+    {
+      id: 2,
+      image: '/images/safety-icon.png',
+      title: 'Безопасный поход',
+      description:
+        'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+    },
+    {
+      id: 3,
+      image: '/images/loyality-icon.png',
+      title: 'Лояльные цены',
+      description:
+        'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+    },
+  ];
 }
