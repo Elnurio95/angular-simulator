3bf606b293b33f3d398458e9e719c3f1010e7827
Elnur <136242453+Elnurio95@users.noreply.github.com>
2026-05-07 19:54:44 +0300

    [Homework №19]: changed user.service and component

--- src/user.service.ts
+++ src/user.service.ts
@@ -2,10 +2,31 @@ import { inject, Injectable } from '@angular/core';
 import { UserApiService } from './user-api.service';
 import { BehaviorSubject, catchError, finalize, Observable, of, tap } from 'rxjs';
 import { IUser } from './interfaces/IUser';
+import { MessageService } from './message.service';
+import { LoaderService } from './loader.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+
+ userApiService: UserApiService = inject(UserApiService);
+  messageService: MessageService = inject(MessageService);
+  loaderService: LoaderService = inject(LoaderService);
   
+  private usersSubject: BehaviorSubject<IUser[]> = new BehaviorSubject<IUser[]>([]);
+  users$: Observable<IUser[]> = this.usersSubject.asObservable();
+
+  setUsers(users: IUser[]): void {
+    this.usersSubject.next(users);
+  }
+
+  getUsers(): Observable<IUser[]> {
+    return this.users$;
+  }
+
+  loadUsers(): Observable<IUser[]> {
+    return this.userApiService.getUsers()
+  }
+
 }
